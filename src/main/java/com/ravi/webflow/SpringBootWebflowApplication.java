d51439ebd6374fa98edb1db67f3c6a8c4082985c
ravik_000 <ravik_000@192.168.1.4>
2017-10-24 12:29:41 +0530

    SpringBoot and Workflow POC

---
+++ src/main/java/com/ravi/webflow/SpringBootWebflowApplication.java
@@ -0,0 +1,12 @@
+package com.ravi.webflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebflowApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ravi/webflow/SpringBootWebflowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ravi.webflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebflowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
