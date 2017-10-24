068d15eb06f7167df2fbdd52c9d35a0b33ae5d40
ravik_000 <ravik_000@192.168.1.4>
2017-10-24 12:31:05 +0530

    Spring Boot & Webflow

--- src/main/java/com/ravi/webflow/SpringBootWebflowApplication.java
+++ src/main/java/com/ravi/webflow/SpringBootWebflowApplication.java
@@ -3,6 +3,11 @@ package com.ravi.webflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author ravik_000
+ *
+ */
 @SpringBootApplication
 public class SpringBootWebflowApplication {
 
